905beaf79e3db2b5429eb549266642f9bd3634b4
unknown <ritsuki-i>
2024-03-28 03:10:21 +0900

    application,system ランダム順序

--- src/AppCard.jsx
+++ src/AppCard.jsx
@@ -1,8 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./AppCard.css";
 import AppData from "./Data/AppData.json";
 
 export default function AppCard() {
+  const [shuffledApps, setShuffledApps] = useState([]);
+  const [shuffledSystems, setShuffledSystems] = useState([]);
+
+  // 配列をシャッフルする関数
+  const shuffleArray = (array) => {
+    let currentIndex = array.length, randomIndex;
+
+    // 配列が空になるまでシャッフルを続ける
+    while (currentIndex !== 0) {
+      // 残っている要素をランダムに取得
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // そしてそれを現在の要素と交換する
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+  };
+
   const adjustFontSize = () => {
     if (window.innerWidth <= 960) {
       const playbtn = document.querySelectorAll(".long");
@@ -25,19 +46,38 @@ export default function AppCard() {
 
   useEffect(() => {
     adjustFontSize();
+    setShuffledApps(shuffleArray([...AppData.apps]));
+    setShuffledSystems(shuffleArray([...AppData.systems]));
   },[]);
   return (
     <div className="AppCard">
       <h1 className="title">Application</h1>
       <div className="row g-4 mt-3">
-        {AppData.apps.map((app, index) => (
-          <div className="col-sm-4" key={index}>
+        {shuffledApps.map((app) => (
+          <div className="col-sm-4" key={app.name}>
             <a href={app.url} target="_blank" rel="noopener noreferrer">
               <div className="app-card">
                 <div className="my-4">
                   <img src={app.path} className="appimg" alt="" />
                   <h5 className="long">{app.name}</h5>
                   <p>{app.script}</p>
+                  <button className="play-btn">Play</button>
+                </div>
+              </div>
+            </a>
+          </div>
+        ))}
+      </div>
+      <h1 className="title">System</h1>
+      <div className="row g-4 mt-3">
+        {shuffledSystems.map((system) => (
+          <div className="col-sm-4" key={system.name}>
+            <a href={system.url} target="_blank" rel="noopener noreferrer">
+              <div className="app-card">
+                <div className="my-4">
+                  <img src={system.path} className="appimg" alt="" />
+                  <h5 className="long">{system.name}</h5>
+                  <p>{system.script}</p>
                   <button className="play-btn">Play</button>
                 </div>
               </div>
